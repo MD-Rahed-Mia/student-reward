05a73a468972c10c73823f0d586e1eab743a0220
Md Rahed Mia <135752931+MD-Rahed-Mia@users.noreply.github.com>
2024-08-21 22:10:45 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+//menu btn
+const menuBtn = document.querySelector("#menu-btn");
+
+//menu container
+const menuContainer = document.querySelector('#menu-container');
+
+//menu icon 
+const menuIcon = document.querySelector('#menu-icon');
+
+
+//setup menu function
+function menuAppear() {
+
+    if (menuContainer.classList.contains('right-0')) {
+        menuContainer.classList.remove('right-0');
+        menuIcon.classList.remove('fa-xmark')
+        menuIcon.classList.add('fa-bars');
+
+        document.body.style.maxHeight = "auto";
+        document.body.style.overflowY = 'scroll';
+    } else {
+        menuContainer.classList.add('right-0');
+        menuIcon.classList.add('fa-xmark');
+        menuIcon.classList.remove('fa-bars');
+
+        document.body.style.maxHeight = "100vh";
+        document.body.style.overflow = 'hidden';
+
+    }
+
+}
+
+menuBtn.addEventListener('click', menuAppear)
